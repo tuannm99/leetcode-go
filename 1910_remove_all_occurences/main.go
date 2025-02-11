17ffc9c178a79ce8f8e915d7b7f94da65da7b3de
Nguyen Minh Tuan <81021902+tuannm99@users.noreply.github.com>
2025-02-11 09:36:23 +0700

    main.go

---
+++ 1910_remove_all_occurences/main.go
@@ -0,0 +1,11 @@
+package main
+
+func removeOccurrences(s string, part string) string {
+    for i := 0; i <= len(s)-len(part); i++ {
+		if s[i:i+len(part)] == part {
+			s = s[0:i] + s[i+len(part):]
+			i = -1
+		}
+	}
+	return s
+}
